d93e2d4330be214d0f14e10e76f697942111f3db
Per Nordlöw <per.nordlow@gmail.com>
2017-03-08 11:27:03 +0100

    Fix

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <map>
 #include <set>
 
+#include <librsvg/rsvg.h>
+
 using std::cout;
 using std::endl;
 using std::hex;
@@ -10,6 +12,15 @@ using std::dec;
 
 int main(int argc, const char * argv[], const char * envp[])
 {
+    RsvgHandle* handle;
+
+    (void)argc;
+    (void)argv;
+
+    g_type_init();
+    handle = rsvg_handle_new();
+    g_object_unref(handle);
+
     cout << "Hello world!" << endl;
     return 0;
 }
